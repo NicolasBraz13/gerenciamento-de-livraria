9275029c5066fdbd44d1be2aade1cff66445c6a1
NicolasBraz13 <167779279+NicolasBraz13@users.noreply.github.com>
2024-04-22 11:07:52 -0300

    Exercicio livro

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace gerenciamento_de_livraria;
+
+class Program
+{
+static void Main(string[] args)
+{
+Livros livroBirdBox= new Livros();
+livroBirdBox.Settitulo("Titulo: Bird Box");
+Console.WriteLine(livroBirdBox.GetNome());
+Console.WriteLine(livroBirdBox.GetAutor());
+Console.WriteLine(livroBirdBox.GetEstoque());
+Console.WriteLine(livroBirdBox.GetIsbn());
+}
+}
